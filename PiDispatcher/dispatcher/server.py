6507a71505c807ad865513191bcb8189277b2b04
AUTHORNAME <AUTHOREMAIL>
2015-03-18 16:13:31 -0700

    wip

--- PiDispatcher/dispatcher/server.py
+++ PiDispatcher/dispatcher/server.py
@@ -1,6 +1,10 @@
 import logging
 import socket
 import asyncio
+import time
+
+from dispatcher.Adafruit_PWM_Servo_Driver import PWM
+
 
 TCP_PORT = 9999
 _log = logging.getLogger(__name__)
@@ -25,9 +29,30 @@ class DispatcherProtocol(asyncio.Protocol):
 def run_server():
     logging.basicConfig(level=logging.DEBUG, filename='dispatcher.log')
     loop = asyncio.get_event_loop()
+
+    _init_servos()
     server = loop.run_until_complete(loop.create_server(
         protocol_factory=DispatcherProtocol,
         port=TCP_PORT,
         family=socket.AF_INET)
     )
     loop.run_until_complete(server.wait_closed())
+
+
+def _init_servos():
+
+    # Initialise the PWM device using the default address
+    pwm = PWM(0x40, debug=True)
+
+    servoMin = 150  # Min pulse length out of 4096
+    servoMax = 600  # Max pulse length out of 4096
+    pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+
+    while (True):
+      # Change speed of continuous servo on channel O
+      pwm.setPWM(0, 0, servoMin)
+      time.sleep(1)
+      pwm.setPWM(0, 0, servoMax)
+      time.sleep(1)
+
+
