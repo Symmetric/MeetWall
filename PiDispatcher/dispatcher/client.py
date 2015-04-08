152a3edaf14908e59492a4d1427239d050c8761c
AUTHORNAME <AUTHOREMAIL>
2015-04-08 15:10:03 -0700

    Implement repeat function in test client

--- PiDispatcher/dispatcher/client.py
+++ PiDispatcher/dispatcher/client.py
@@ -20,10 +20,11 @@ def send_to_server():
 
     1,23, 22, 14, 14 , 1, 255, 0
 
-    Usage: dispatcher_client [--ip=<IP>] MESSAGE
+    Usage: dispatcher_client [--ip=<IP>] [-r] MESSAGE
 
     Options:
         --ip=<IP>  The IP to connect to [default: 127.0.0.1]
+        -r         Repeat the message sequence indefinitely
     """
     set_logging(_log, filename='dispatcher_client.log')
     args = docopt.docopt(send_to_server.__doc__, help=False)
@@ -35,17 +36,24 @@ def send_to_server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, TCP_PORT))
 
-    for message in messages:
-        # Split into a list of str values, trimming whitespace to allow easy parsing.
-        input_numbers = [int(s) for s in message.replace(' ', '').split(',')]
-        input_string = string.join([chr(b) for b in input_numbers], '')
-        if len(input_string) < 160:
-            missing_bytes = 160 - len(input_string)
-            input_string += chr(0) * missing_bytes
+    send_message = True
 
-        output_bytes = [ord(b) for b in input_string]
-        _log.info('Sending to server %s, len=%d: %s', ip, len(output_bytes), output_bytes)
-        sock.send(input_string)
-        time.sleep(1)
+    while send_message:
+        for message in messages:
+            # Split into a list of str values, trimming whitespace to allow easy parsing.
+            input_numbers = [int(s) for s in message.replace(' ', '').replace('\n', '').split(',')]
+            input_string = string.join([chr(b) for b in input_numbers], '')
+            if len(input_string) < 160:
+                missing_bytes = 160 - len(input_string)
+                input_string += chr(0) * missing_bytes
+
+            output_bytes = [ord(b) for b in input_string]
+            _log.info('Sending to server %s, len=%d: %s', ip, len(output_bytes), output_bytes)
+            sock.send(input_string)
+            time.sleep(0.1)
+
+        # If we're not in repeat mode, terminate.
+        if not args['-r']:
+            send_message = False
 
     sock.close()
